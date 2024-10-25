2d9e2baaca9ce4d5c08d38fd556eaf7c95556c10
medineshkumarm <medineshkumarm@gmail.com>
2024-10-25 21:31:08 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const Document = require("./document");
+
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose
+  .connect(MONGODB_URI)
+  .then((res) => console.log("db connected"))
+  .catch((err) => console.log(err));
+
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+const defaultValue = "";
+
+io.on("connection", (socket) => {
+  socket.on("get-document", async (documentId) => {
+    const document = await findOrCreateDocument(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
+    });
+  });
+});
+
+async function findOrCreateDocument(id) {
+  if (id == null) return;
+
+  const document = await Document.findById(id);
+  if (document) return document;
+  return await Document.create({ _id: id, data: defaultValue });
+}
